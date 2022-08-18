a235aa5283b0f08665cb23f4149c178ef455317e
Madis <kalashnikov.madis@gmail.com>
2022-08-18 15:22:23 +0300

    Finish third task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	const arr = string.split("")
+	const newArr = []
+	for (let i = string.length - 1; i >= 0; i--) {
+		newArr.push(arr[i])
+	}
+	return newArr.join("")
 };
 
 // Do not edit below this line
