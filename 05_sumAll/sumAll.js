135a2c1c3e091c9243d0f583d34800de2ad48faa
Madis <kalashnikov.madis@gmail.com>
2022-08-18 16:33:12 +0300

    Finish fifth task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+	// initialize result as 0
+	let result = 0;
+	// if numbers are smaller than 0 or not numbers
+	if (typeof n1 !== "number" || typeof n2 !== "number") {
+		return "ERROR"
+	} else if (n1 < 0 || n2 < 0) {
+		return "ERROR"
+	};
+	// if n1 is bigger than n2
+	if (n1 > n2) {
+		while (n1 >= n2) {
+			result += n1;
+			n1--;
+		}
+	// if n1 is smaller than n2
+	} else if (n1 < n2) {
+		while (n1 <= n2) {
+			result += n1;
+			n1++;
+		}
+	}
+	
+	return result;
 };
 
 // Do not edit below this line
