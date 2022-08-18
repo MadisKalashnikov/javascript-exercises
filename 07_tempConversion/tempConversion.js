544263b075fc485560cc4d09a2b9c140c8fc2735
Madis <kalashnikov.madis@gmail.com>
2022-08-18 17:11:00 +0300

    Finish seventh task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let temp1 = temp - 32
+  let calc = 5 / 9
+  return parseFloat((temp1 * calc).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let temp1 = temp * (9 / 5)
+  let calc = temp1 + 32
+  return parseFloat(calc.toFixed(1))
 };
 
 // Do not edit below this line
