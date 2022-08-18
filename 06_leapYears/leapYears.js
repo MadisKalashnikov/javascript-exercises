3adaa9631c3c9dedd38b58ce2eea2cfbf0c547e1
Madis <kalashnikov.madis@gmail.com>
2022-08-18 16:42:50 +0300

    Finish sixth task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	// if year divisible by 4
+	if (year % 4 === 0) {
+		// if divisible by four and 100
+		if (year % 100 === 0) {
+			// if divisiblie by four and hundred and 400
+			if (year % 400 === 0) {
+				return true
+			}
+			return false
+		}
+		return true
+	// if not divisble by 4
+	} else if (year % 4 !== 0) {
+		return false
+	}
 };
 
 // Do not edit below this line
